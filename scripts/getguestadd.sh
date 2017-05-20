f4f635c5643913ed471ae270ea05ed2ef7aa0ad4
Leroy <GitableRoy@users.noreply.github.com>
2017-05-20 18:55:30 -0400

    Vars (#1)
    * Completed basic command options, set variables, and updated README.md
    
    * Fixed restore option cases
    
    * Added app building feature, variables completed

--- scripts/getguestadd.sh
+++ scripts/getguestadd.sh
@@ -2,20 +2,21 @@
 
 # this script gets the VBoxGuestAdditions right for your VM
 
-vm_name=$1
-version=$2
+dest=$1
+vm_name=$2
+version=$3
 
 echo "download this machine's VBoxGuestAdditions"
-curl -o machines/$vm_name/VBoxGuestAdditions_$version.iso -C - -O \
+curl -o $dest/machines/$vm_name/VBoxGuestAdditions_$version.iso -C - -O \
  http://download.virtualbox.org/virtualbox/$version/VBoxGuestAdditions_$version.iso
 
 echo "Using SHA256SUMS to check legitimacy"
-sum=$(shasum -a 256 machines/$vm_name/VBoxGuestAdditions_$version.iso | cut -d " " -f1)
+sum=$(shasum -a 256 $dest/machines/$vm_name/VBoxGuestAdditions_$version.iso | cut -d " " -f1)
 
-check=$(curl http://download.virtualbox.org/virtualbox/$version/SHA256SUMS | grep $sum)
+check=$(curl http://download.virtualbox.org/virtualbox/$version/SHA256SUMS | grep $sum | cut -d " " -f1)
 if ! [ -z $check ]; then
   echo "Safe GuestAdditions downloaded"
 else
   echo "Unsafe GuestAdditions downloaded"
-  rm machines/$vm_name/VBoxGuestAdditions_$version.iso
+  rm $dest/machines/$vm_name/VBoxGuestAdditions_$version.iso
 fi
--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # Commands to destroy VBoxManage
-path=$1
+dest=$1
 vm_name=$2
 
 echo "Set to remove: "$vm_name
 exist=$(VBoxManage list vms | grep $vm_name)
 if ! [ -z $exist ]; then
   VBoxManage unregistervm $vm_name --delete
-  echo $vm_name" has been removed"
-  rm -rf $path/$vm_name
-  echo $vm_name" folder has been removed"
+  echo $vm_name" has been removed from "$dest
+  rm -rf $dest/machines/$vm_name
+  echo $vm_name" folder has been removed from "$dest
+  rm -rf $dest/apps/$vm_name.app
+  echo $vm_name".app has been removed from "$dest
 else
   echo $vm_name" box already doesn't exist, removing subsequent folder"
-  rm -rf $path/$vm_name
+  rm -rf $dest/machines/$vm_name
   echo $vm_name" folder has been removed"
+  rm -rf $dest/apps/$vm_name.app
+  echo $vm_name".app has been removed from "$dest
 fi
