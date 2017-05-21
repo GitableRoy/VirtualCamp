626a5fe04d2cdf16cef0c3b6b1b7298a6096e2ed
Leroy Leslie <GitableRoy@users.noreply.github.com>
2017-05-20 23:26:49 -0400

    Added tab completion feature, made apps and machines visible, added misc folder (#2)

---
+++ misc/vcamp-complete.sh
@@ -0,0 +1,57 @@
+# Tab completion for vcamp.h
+
+_vcamp_tab_complete ()
+{
+  local curr prev
+
+  COMPREPLY=()
+  curr=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+  case "$curr" in
+    *)
+      local opts="create remove restore"
+      COMPREPLY=( $( compgen -W "${opts}" ${curr} ) )
+      ;;
+  esac
+
+  case "$prev" in
+    create)
+      case $curr in
+        *)
+          local subopts="default -n -d -p -e -g --name --dest --part --efi --guest"
+          COMPREPLY=( $( compgen -W "${subopts}" -- ${curr} ) )
+          ;;
+      esac
+      ;;
+
+   remove)
+     case $curr in
+       *)
+         local subopts="default -n -d -p --name --dest --part"
+         COMPREPLY=( $( compgen -W "${subopts}" -- ${curr} ) )
+         ;;
+     esac
+     ;;
+   restore)
+     case $curr in
+       *)
+         local subopts="-p --part"
+         COMPREPLY=( $( compgen -W "${subopts}"  -- ${curr} ) )
+         ;;
+     esac
+     ;;
+    -d|--dest|-p|--part)
+      COMPREPLY=( $(compgen -A directory))
+      return 0
+      ;;
+    default|-e|--efi|-g|--guest|-n|--name)
+      COMPREPLY=()
+      return 0
+      ;;
+  esac
+
+  return 0
+}
+
+complete -F _vcamp_tab_complete -o filenames ../vcamp.sh
